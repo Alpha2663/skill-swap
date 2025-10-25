1b95d00ad6f9981e86d3b326ff760df012137251
Alpha2663 <sudiptorahman35@gmail.com>
2025-10-25 20:56:33 +0600

    added functionality

--- src/components/HowItWorks.jsx
+++ src/components/HowItWorks.jsx
@@ -8,17 +8,18 @@ export default function HowItWorks() {
         <h2 className=" animate__animated animate__rotateIn animate__infinite animate__delay-0s text-3xl font-bold mb-6 text-gray-800  ">
          How It Works
         </h2>
-        <div className="flex justify-center gap-10 text-3xl font-bold text-yellow-600">
-          <p className=" animate__animated animate__bounce animate__infinite animate__delay-0s">
-         <FaArrowRight /> Swap 
-          </p>
-          <p className="animate__animated animate__bounce animate__infinite animate__delay-1s">
-           <FaArrowRight /> Session
-          </p>
-          <p className="animate__animated animate__bounce animate__infinite animate__delay-2s">
-           <FaArrowRight /> Exchange
-            
-          </p>
+        <div className="flex flex-wrap justify-center items-center gap-6 sm:gap-8 md:gap-10 text-lg sm:text-2xl md:text-3xl font-bold text-yellow-600 text-center px-4 py-6">
+        <p className="flex items-center gap-2 animate__animated animate__bounce animate__infinite animate__delay-0s">
+        <FaArrowRight /> Swap
+        </p>
+
+        <p className="flex items-center gap-2 animate__animated animate__bounce animate__infinite animate__delay-1s">
+        <FaArrowRight /> Session
+        </p>
+
+        <p className="flex items-center gap-2 animate__animated animate__bounce animate__infinite animate__delay-2s">
+        <FaArrowRight /> Exchange
+         </p>
         </div>
       </div>
     </section>
